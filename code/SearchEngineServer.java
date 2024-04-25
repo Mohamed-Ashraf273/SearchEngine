4c74552cac375f2470e38bffcd366fc680bbfcdb
Mohamed-Ashraf273 <ma2736666@gmail.com>
2024-04-25 19:30:42 +0200

    RemainingPageRank

--- code/SearchEngineServer.java
+++ code/SearchEngineServer.java
@@ -19,6 +19,7 @@ public class SearchEngineServer {
         String url;
         double TF;// normalized
         double TF_IDF;
+        double Tf_IDF_total;// if there is more than one word
         double PageRank;
     }
 
@@ -51,21 +52,25 @@ public class SearchEngineServer {
             // Initialize each doc object within the docs array
             for (int j = 0; j < wrds[i].docs.length; j++) {
                 wrds[i].docs[j] = new doc(); // Create a new doc object
+                wrds[i].docs[j].Tf_IDF_total = 0;
+                wrds[i].docs[j].TF_IDF = 0;
+                wrds[i].docs[j].TF = 0;
+                wrds[i].docs[j].PageRank = 0;
             }
         }
         wrds[0].DF = 10.0 / 30.0;
         wrds[0].wrd = "football";
-        wrds[0].docs[0].url = "https://www.bbc.com/sport/football";
-        wrds[0].docs[1].url = "https://en.wikipedia.org/wiki/Football";
-        wrds[0].docs[2].url = "https://www.skysports.com/football";
+        wrds[0].docs[0].url = "https://www.crazygames.com/t/soccer";
+        wrds[0].docs[1].url = "https://playfootball.games/";
+        wrds[0].docs[2].url = "https://poki.com/en/american-football";
         wrds[0].docs[3].url = "https://www.theguardian.com/football";
         wrds[0].docs[4].url = "https://www.britannica.com/sports/football-soccer";
         wrds[0].docs[5].url = "https://www.aljazeera.com/sports/liveblog/2024/4/23/live-arsenal-vs-chelsea-premier-league-football";
         wrds[0].docs[6].url = "https://en.wikipedia.org/wiki/Association_football";
-        wrds[0].docs[7].url = "https://www.goal.com/en/live-scores";
-        wrds[0].docs[8].url = "https://onefootball.com/";
-        wrds[0].docs[9].url = "https://www.bbc.com/sport/football/scores-fixtures";
-        for (int i = 0; i < 10; i++) {
+        wrds[0].docs[7].url = "https://www.bbc.com/sport/football";
+        wrds[0].docs[8].url = "https://en.wikipedia.org/wiki/Football";
+        wrds[0].docs[9].url = "https://www.skysports.com/football";
+        for (int i = 0; i < 3; i++) {
             wrds[0].docs[i].TF = calc_tf(wrds[0].wrd, wrds[0].docs[i].url);
             System.out.println(wrds[0].docs[i].TF);
             wrds[0].docs[i].TF_IDF = calc_tfIdf(wrds[0].docs[i].url, wrds[0].DF, wrds[0].docs[i].TF);
@@ -91,21 +96,21 @@ public class SearchEngineServer {
 
         wrds[2].DF = 10 / 30;
         wrds[2].wrd = "games";
-        wrds[2].docs[0].url = "https://en.wikipedia.org/wiki/Video_game";
-        wrds[2].docs[1].url = "https://www.amazon.com/computer-video-games-hardware-accessories/b?ie=UTF8&node=468642";
-        wrds[2].docs[2].url = "https://www.metacritic.com/browse/game/";
+        wrds[2].docs[0].url = "https://playfootball.games/";
+        wrds[2].docs[1].url = "https://www.crazygames.com/t/soccer";
+        wrds[2].docs[2].url = "https://poki.com/en/american-football";
         wrds[2].docs[3].url = "https://www.gamespot.com/";
         wrds[2].docs[4].url = "https://www.wired.com/tag/video-games/";
         wrds[2].docs[5].url = "https://www.metacritic.com/browse/game/all/all/current-year/";
         wrds[2].docs[6].url = "https://www.circana.com/industry-expertise/video-games/";
         wrds[2].docs[7].url = "https://videogamesplus.ca/";
         wrds[2].docs[8].url = "https://www.target.com/c/video-games/-/N-5xtg5";
-        wrds[2].docs[9].url = "https://en.wikipedia.org/wiki/History_of_video_games";
-        // for (int i = 0; i < 10; i++) {
-        // wrds[2].docs[i].TF = calc_tf(wrds[2].wrd, wrds[2].docs[i].url);
-        // wrds[2].docs[i].TF_IDF = calc_tfIdf(wrds[2].docs[i].url, wrds[2].DF,
-        // wrds[2].docs[i].TF);
-        // }
+        wrds[2].docs[9].url = "https://en.wikipedia.org/wiki/Video_game";
+        for (int i = 0; i < 3; i++) {
+            wrds[2].docs[i].TF = calc_tf(wrds[2].wrd, wrds[2].docs[i].url);
+            wrds[2].docs[i].TF_IDF = calc_tfIdf(wrds[2].docs[i].url, wrds[2].DF,
+                    wrds[2].docs[i].TF);
+        }
         System.out.println("finished");
         waitTillCalc = false;
         // --------------------------------------------------------------------------------
@@ -116,7 +121,7 @@ public class SearchEngineServer {
         public void handle(HttpExchange exchange) throws IOException {
             if ("GET".equals(exchange.getRequestMethod())) {
                 String query = exchange.getRequestURI().getQuery();// query words from the user in a string form
-                String[] finalList = new String[10];// modify the size to be the final list size
+                String[] finalList = null;
                 while (waitTillCalc) {
                     try {
                         TimeUnit.SECONDS.sleep(1); // Delay for 5 seconds
@@ -125,20 +130,73 @@ public class SearchEngineServer {
                     }
                 }
                 // Perform operations based on the query
+                word[] words_have_common_docs;
                 if ("football".equals(query)) {
-                    Ranker(wrds[0].docs);
-                    for (int i = 0; i < 10; i++) {
-                        finalList[i] = wrds[0].docs[i].url;
+                    words_have_common_docs = new word[1];
+                    words_have_common_docs[0] = new word();
+                    words_have_common_docs[0] = wrds[0];
+                    doc[] Docs = new doc[words_have_common_docs[0].docs.length];
+                    for (int i = 0; i < Docs.length; i++) {
+                        Docs[i] = new doc();
                     }
+                    Ranker(words_have_common_docs, Docs);
+                    finalList = new String[Docs.length];// modify the size to be the final list size
+                    for (int i = 0; i < Docs.length; i++) {
+                        finalList[i] = Docs[i].url;
+                    }
+                    // for (String url : finalList) {
+                    // System.out.println(url);
+                    // }
                 } else if ("food".equals(query)) {
-                    Ranker(wrds[1].docs);
-                    for (int i = 0; i < wrds[1].docs.length; i++) {
-                        finalList[i] = wrds[1].docs[i].url;
-                    }
+                    words_have_common_docs = new word[1];
+                    words_have_common_docs[0] = new word();
+                    words_have_common_docs[0] = wrds[1];
+                    // doc[] Docs = Ranker(words_have_common_docs);
+                    // for (int i = 0; i < Docs.length; i++) {
+                    // finalList[i] = Docs[i].url;
+                    // }
                 } else {
-                    Ranker(wrds[2].docs);
-                    for (int i = 0; i < wrds[2].docs.length; i++) {
-                        finalList[i] = wrds[2].docs[i].url;
+                    if ("Games".equals(query)) {
+                        words_have_common_docs = new word[1];
+                        words_have_common_docs[0] = new word();
+                        words_have_common_docs[0] = wrds[2];
+                        // doc[] Docs = Ranker(words_have_common_docs);
+                        // for (int i = 0; i < Docs.length; i++) {
+                        // finalList[i] = Docs[i].url;
+                        // }
+                    } else {
+                        words_have_common_docs = new word[2];
+                        words_have_common_docs[0] = new word();
+                        words_have_common_docs[0].DF = 3;
+                        words_have_common_docs[0].wrd = "football";
+                        words_have_common_docs[0].docs = new doc[3];
+                        words_have_common_docs[0].docs[0] = new doc();
+                        words_have_common_docs[0].docs[0] = wrds[0].docs[0];
+                        words_have_common_docs[0].docs[1] = new doc();
+                        words_have_common_docs[0].docs[1] = wrds[0].docs[1];
+                        words_have_common_docs[0].docs[2] = new doc();
+                        words_have_common_docs[0].docs[2] = wrds[0].docs[2];
+
+                        words_have_common_docs[1] = new word();
+                        words_have_common_docs[1].DF = 3;
+                        words_have_common_docs[1].wrd = "Games";
+                        words_have_common_docs[1].docs = new doc[3];
+                        words_have_common_docs[1].docs[0] = new doc();
+                        words_have_common_docs[1].docs[0] = wrds[2].docs[0];
+                        words_have_common_docs[1].docs[1] = new doc();
+                        words_have_common_docs[1].docs[1] = wrds[2].docs[1];
+                        words_have_common_docs[1].docs[2] = new doc();
+                        words_have_common_docs[1].docs[2] = wrds[2].docs[2];
+
+                        doc[] Docs = new doc[words_have_common_docs[0].docs.length];
+                        for (int i = 0; i < Docs.length; i++) {
+                            Docs[i] = new doc();
+                        }
+                        Ranker(words_have_common_docs, Docs);
+                        finalList = new String[Docs.length];// modify the size to be the final list size
+                        for (int i = 0; i < Docs.length; i++) {
+                            finalList[i] = Docs[i].url;
+                        }
                     }
                 }
 
@@ -215,18 +273,42 @@ public class SearchEngineServer {
     }
 
     // Ranker function
-    public static void Ranker(doc[] docs) {
+    public static void Ranker(word[] words, doc[] ds) {
+        System.out.println("hello from ranker");
+        for (int i = 0; i < ds.length; i++) {
+            ds[i].Tf_IDF_total = words[0].docs[i].TF_IDF;
+            ds[i].url = words[0].docs[i].url;
+        }
+        // for (int i = 0; i < ds.length; i++) {
+        // System.out.println(ds[i].url);
+        // }
         // note i use array of docs to attach each url to its data
-        int size = docs.length;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size - 1; j++) {
-                if (docs[j].TF_IDF > docs[j + 1].TF_IDF) {
-                    doc temp = docs[j];
-                    docs[j] = docs[j + 1];
-                    docs[j + 1] = temp;
+        // note if there is more than 1 element in the array it means that the query has
+        // many words then they all must have the same number of docs as they will store
+        // the common docs between them
+        for (int k = 0; k < words[0].docs.length; k++) {// for each url in words[0]
+            for (int i = 1; i < words.length; i++) {// search in all words docs about this url and add all tf_idf and
+                                                    // store it in a doc
+                for (int j = 0; j < words[i].docs.length; j++) {// iterate over the docs in words[i]
+                    if (words[0].docs[k].url.equals(words[i].docs[j].url)) {
+                        ds[k].Tf_IDF_total += words[i].docs[j].TF_IDF;
+                        break;
+                    }
                 }
             }
         }
+        for (int i = 0; i < ds.length; i++) {
+            for (int j = 0; j < ds.length - 1; j++) {
+                if (ds[j + 1].Tf_IDF_total > ds[j].Tf_IDF_total) {
+                    doc temp = ds[j + 1];
+                    ds[j + 1] = ds[j];
+                    ds[j] = temp;
+                }
+            }
+        }
+        // for (int i = 0; i < ds.length; i++) {
+        // System.out.println(ds[i].url);
+        // }
         return;
     }
 }
